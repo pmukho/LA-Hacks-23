083a33678a4410446a673d3d29c0070047a1cfe6
pmukho <pmukho2003@gmail.com>
2023-04-22 01:10:02 -0700

    server.js now listens to port

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 
+const express = require('express');
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
+
+const app = express();
+app.use(express.json());
 
 mongoose
     .connect(process.env.MONGODB_URI,
@@ -11,5 +15,7 @@ mongoose
             useUnifiedTopology: true,
         }
     )
-    .then(() => console.log("Connected to DB"))
+    .then(() => console.log('Connected to DB'))
     .catch(console.error);
+
+app.listen(3001, () => console.log('Server listening on port 3001: http://localhost:3001'));
